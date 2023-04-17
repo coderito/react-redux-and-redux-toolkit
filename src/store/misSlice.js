b061949462a44fc175abcff85ce3a460eb4d1aa5
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-04-17 19:52:59 -0400

    agregando propiedades

--- src/store/misSlice.js
+++ src/store/misSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from "@reduxjs/toolkit";
 // funcion encarga de actualizar
 export const origenSlice = createSlice({
     name: "unValor",
-    initialState: {miNombre: "Juan"},
+    initialState: {miNombre: "Juan", miApellido: "Eulufi"},
     reducers: {
         guardarNombre: (state, action) => {
             // enviando action y guardarlo en el estado
