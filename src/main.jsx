bcdebf7ded63925f41ebd62dee4f520b29b39710
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-04-17 18:37:35 -0400

    implementando BrowserRouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
 import './index.css'
+import MiRouter from './MiRouter'
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <MiRouter />
+    </BrowserRouter>
   </React.StrictMode>,
 )
