692a94d4cd1f9c8b393ea7b9fb07161e87130d0a
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-04-17 19:53:40 -0400

    agregando ( Provider store)

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import MiRouter from './MiRouter'
 import { BrowserRouter } from 'react-router-dom'
+import store from './store/store'
+import { Provider } from 'react-redux'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <MiRouter />
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <MiRouter />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 )
