467ec595d14f644f69ac46755836d5e33dfb797f
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-04-17 18:57:17 -0400

    creando slice ( createSlice )

---
+++ src/store/misSlice.js
@@ -0,0 +1,15 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// funcion encarga de actualizar
+export const origenSlice = createSlice({
+    name: "unValor",
+    initialState: {miNombre: "Juan"},
+    reducers: {
+        guardarNombre: (state, action) => {
+            // enviando action y guardarlo en el estado
+            state.miNombre = action.payload;
+        }
+    }
+})
+
+export const {guardarNombre} = origenSlice.actions
