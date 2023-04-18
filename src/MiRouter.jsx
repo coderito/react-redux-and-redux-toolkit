8fe884a414bc684f6ec8e18f8e9d4c6d0dfde373
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-04-18 12:36:08 -0400

    creando nuevo router

--- src/MiRouter.jsx
+++ src/MiRouter.jsx
@@ -2,19 +2,25 @@ import React, { Fragment } from "react";
 import { Router, Routes, Route, Link } from "react-router-dom";
 import Uno from "./components/Uno";
 import Dos from "./components/Dos";
+import Listado from "./components/Listado";
+import Candidato from "./components/Candidato";
+import Gestion from "./components/Gestion";
 
 export default function MiRouter() {
   return (
     <div>
-      <Link to="/uno">
+      {/* <Link to="/uno">
         <button>Ir a uno</button>
       </Link>
       <Link to="/dos">
         <button>Ir a dos</button>
-      </Link>
+      </Link> */}
       <Routes>
-        <Route path="/uno" element={<Uno />} />
-        <Route path="/dos" element={<Dos />} />
+        <Route path="/" element={<Listado />} />
+        {/* <Route path="/uno" element={<Uno />} />
+        <Route path="/dos" element={<Dos />} /> */}
+        <Route path="/candidato" element={<Candidato/>} />
+        <Route path="/gestion" element={<Gestion />} />
       </Routes>
     </div>
   );
---
+++ src/components/Listado.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Listado() {
+  return (
+    <div>Listado</div>
+  )
+}
