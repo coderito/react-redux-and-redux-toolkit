7c44d8400089461acf7170782983c25f6f67b6a8
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-04-17 20:01:57 -0400

    usando dispatch

--- src/components/Uno.jsx
+++ src/components/Uno.jsx
@@ -1,15 +1,20 @@
-import React from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { guardarNombre } from "../store/misSlice";
 
 export default function Uno() {
-    // enviar informacion a un reducer
-    const dispatch = useDispatch();
-    // seleccion un estado
-    const miNombreAhora = useSelector(state => state.unValor.miNombre);
+  // enviar informacion a un reducer
+  const dispatch = useDispatch();
+  // seleccion un estado
+  const miNombreAhora = useSelector((state) => state.unValor.miNombre);
 
+  const modificar = () => {
+    dispatch(guardarNombre("Bob Sponja"));
+  };
   return (
-    <h1>
-        {miNombreAhora}
-    </h1>
-  )
+    <div>
+      <h1>{miNombreAhora}</h1>
+      <button onClick={modificar}>Modificar nombre</button>
+    </div>
+  );
 }
