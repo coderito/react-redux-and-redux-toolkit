d9449ed22ebbcfe7749020736a6b45e5442b8a1f
Juan Eulufi <juan.eulufi.sa@gmail.com>
2023-04-18 12:45:29 -0400

    fetch a la api y mapeando resultados

--- src/components/Listado.jsx
+++ src/components/Listado.jsx
@@ -1,7 +1,21 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 
 export default function Listado() {
+  const [cantidatos, setCantidatos] = useState([]);
+
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=6")
+      .then((res) => res.json())
+      .then((data) => setCantidatos(data.results));
+  }, []);
+
   return (
-    <div>Listado</div>
-  )
+    <div>
+      <h1>{cantidatos.length}</h1>
+
+      {cantidatos.map((item) => (
+        <p>{item.name.first}</p>
+      ))}
+    </div>
+  );
 }
